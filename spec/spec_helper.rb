8383aa46af46d6272416121c3d34f5a262a54de3
byplayer <byplayer100@gmail.com>
2017-10-26 13:12:28 +0800

    support rcov and suppress rubocop warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,18 @@
-# -*- coding: utf-8 -*-
-require 'rspec'
+# frozen_string_literal: true
 
-$:.unshift File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
-Dir[File.dirname(__FILE__) + "/supports/**/*.rb"].each {|f| require f}
+require 'rspec'
+require 'simplecov'
+
+$LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__),
+                                              '..', 'lib'))
+Dir[File.dirname(__FILE__) + '/supports/**/*.rb'].each { |f| require f }
 
 require 'roma-client'
+
+SimpleCov.start do
+  add_filter '/vendor/'
+end
 
 RSpec.configure do |config|
   config.mock_with :rr
 end
-
